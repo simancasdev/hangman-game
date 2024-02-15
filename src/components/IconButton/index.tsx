3c8d69ede9990c4189761d4043448c7ed3811abc
“simancasdev” <129432908+simancasdev@users.noreply.github.com>
2024-02-15 19:49:24 -0400

    extending IconButton component props

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -1,8 +1,10 @@
 import {Children} from "types";
 import {Button} from "./styled";
 
-interface IconButtonProps extends Children<JSX.Element> {}
+interface IconButtonProps extends Children<JSX.Element> {
+  onClick: () => void;
+}
 
-export const IconButton: React.FC<IconButtonProps> = ({children}) => {
-  return <Button children={children} />;
+export const IconButton: React.FC<IconButtonProps> = ({children, onClick}) => {
+  return <Button children={children} onClick={onClick} />;
 };
