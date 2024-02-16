398534e9c77a1ee71cf663e4d28c23529667f903
“simancasdev” <129432908+simancasdev@users.noreply.github.com>
2024-02-15 22:31:33 -0400

    updating menu component

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,27 +1,52 @@
 import {useUI} from "context";
 import {Fragment} from "react";
 import {Container} from "./styled";
+import {Moon, Sun} from "react-feather";
 import {Backdrop} from "components/Backdrop";
-import {Toggle, Typography} from "components";
+import {Box, Toggle, Typography} from "components";
 
 interface MenuProps {}
 
 export const Menu: React.FC<MenuProps> = () => {
-  const {menuOpened, defaultTheme, onMenu, toggleTheme} = useUI();
+  const {
+    language,
+    menuOpened,
+    defaultTheme,
+    onMenu,
+    toggleTheme,
+    toggleLanguage,
+  } = useUI();
   return menuOpened ? (
     <Fragment>
       <Backdrop onClick={() => onMenu(false)} />
       <Container>
-        <Typography color="#fff" fontWeight="bold">
-          Cambiar tema
-        </Typography>
-        <Toggle
-          style={{marginTop: 10}}
-          isOn={defaultTheme === "dark"}
-          onToggle={() =>
-            toggleTheme(defaultTheme === "dark" ? "light" : "dark")
-          }
-        />
+        <Box flexDirection="column" gap={20}>
+          <Typography fontSize={25} fontWeight="bold">
+            Menú del juego
+          </Typography>
+          <Box alignItems="center" gap={10}>
+            <Typography fontSize={18} fontWeight="bold">
+              ES
+            </Typography>
+            <Toggle
+              isOn={language === "en"}
+              onToggle={() => toggleLanguage(language === "en" ? "es" : "en")}
+            />
+            <Typography fontSize={18} fontWeight="bold">
+              EN
+            </Typography>
+          </Box>
+          <Box alignItems="center" gap={10}>
+            <Moon />
+            <Toggle
+              isOn={defaultTheme === "dark"}
+              onToggle={() =>
+                toggleTheme(defaultTheme === "dark" ? "light" : "dark")
+              }
+            />
+            <Sun />
+          </Box>
+        </Box>
       </Container>
     </Fragment>
   ) : (
--- src/components/Menu/styled.ts
+++ src/components/Menu/styled.ts
@@ -6,8 +6,8 @@ export const Container = styled.div`
   right: 1vw;
   background-color: rgba(40, 44, 52, 1);
   padding: 2rem 1rem;
-  width: 400px;
+  width: 300px;
   border-radius: 1rem;
   z-index: 2;
-  box-shadow: 5px 5px 3px 1px rgba(0, 0, 0, 0.2);
+  box-shadow: -5px 5px 3px 1px rgba(0, 0, 0, 0.2);
 `;
