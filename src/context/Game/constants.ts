b4edcb9f6eda013a449d100ae2d6351a7811ae50
“simancasdev” <129432908+simancasdev@users.noreply.github.com>
2024-02-15 22:46:32 -0400

    improving game constants

--- src/context/Game/constants.ts
+++ src/context/Game/constants.ts
@@ -1,5 +1,8 @@
+import {PARTS as GUY_PARTS} from "components/Drawing/lib/Guy/helper";
+import {PARTS as TREE_PARTS} from "components/Drawing/lib/Tree/helper";
+
 export const INITIAL_GAME_DURATION = 60; // <-- seconds
-export const ATTEMPTS_TO_COMPLETE_GUY_DRAWING = 7;
-export const ATTEMPTS_TO_COMPLETE_TREE_DRAWING = 3;
+export const ATTEMPTS_TO_COMPLETE_GUY_DRAWING = GUY_PARTS.length;
+export const ATTEMPTS_TO_COMPLETE_TREE_DRAWING = TREE_PARTS.length;
 export const ATTEMPTS_TO_COMPLETE_DRAWING =
   ATTEMPTS_TO_COMPLETE_GUY_DRAWING + ATTEMPTS_TO_COMPLETE_TREE_DRAWING;
