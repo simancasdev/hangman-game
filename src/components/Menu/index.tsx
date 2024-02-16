d2b8e2640d96ccc186a1df4400d4ac3b3f9e8a8e
“simancasdev” <129432908+simancasdev@users.noreply.github.com>
2024-02-15 20:09:47 -0400

    working on menu component

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,15 +2,27 @@ import {useUI} from "context";
 import {Fragment} from "react";
 import {Container} from "./styled";
 import {Backdrop} from "components/Backdrop";
+import {Toggle, Typography} from "components";
 
 interface MenuProps {}
 
 export const Menu: React.FC<MenuProps> = () => {
-  const {menuOpened, onMenu} = useUI();
+  const {menuOpened, defaultTheme, onMenu, toggleTheme} = useUI();
   return menuOpened ? (
     <Fragment>
       <Backdrop onClick={() => onMenu(false)} />
-      <Container />
+      <Container>
+        <Typography color="#fff" fontWeight="bold">
+          Cambiar tema
+        </Typography>
+        <Toggle
+          style={{marginTop: 10}}
+          isOn={defaultTheme === "dark"}
+          onToggle={() =>
+            toggleTheme(defaultTheme === "dark" ? "light" : "dark")
+          }
+        />
+      </Container>
     </Fragment>
   ) : (
     <Fragment />
--- src/components/Menu/styled.ts
+++ src/components/Menu/styled.ts
@@ -4,10 +4,10 @@ export const Container = styled.div`
   position: fixed;
   top: 80px;
   right: 1vw;
-  background-color: red;
+  background-color: rgba(40, 44, 52, 1);
+  padding: 2rem 1rem;
   width: 400px;
   border-radius: 1rem;
-  height: 500px;
   z-index: 2;
-  box-shadow: 2px 3px 3px 1px rgba(0, 0, 0, 0.1);
+  box-shadow: 5px 5px 3px 1px rgba(0, 0, 0, 0.2);
 `;
